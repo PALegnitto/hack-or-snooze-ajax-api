d78324174da79aeb6678d46d1383402ade5ac1a2
Phill Legnitto <palegnitto@gmail.com>
2022-07-21 16:29:43 -0700

    Create add new story functionality and post story functionality

--- js/stories.js
+++ js/stories.js
@@ -53,6 +53,26 @@ function putStoriesOnPage() {
 
 /** */
 
-function createStoryFormAndAddNewStory() {
+async function addNewStory(evt) {
+  console.debug("addNewStory");
+  evt.preventDefault();
+  
+  const storyAuthorVal = $("#story-author").val()
+  const storyTitleVal = $("#story-title").val()
+  const storyUrlVal = $("#story-url").val()
 
+  const newFormVals = {
+    author: storyAuthorVal,
+    title: storyTitleVal, url: storyUrlVal
+  }
+  
+  const newStory =  await storyList.addStory(currentUser, newFormVals)
+  $storySubmit.trigger("reset");
+
+  console.log(newStory);
 }
+
+$storyForm.on("submit", addNewStory)
+
+
+
